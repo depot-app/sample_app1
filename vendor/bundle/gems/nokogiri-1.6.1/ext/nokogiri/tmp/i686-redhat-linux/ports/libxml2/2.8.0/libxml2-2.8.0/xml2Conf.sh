b31055ad3c1389f1d8a068c2e2c9afb469e2de18
Juan Ruiz <invitado@perdidita.(none)>
2014-04-01 13:08:23 +0100

    Initialize repository

---
+++ vendor/bundle/gems/nokogiri-1.6.1/ext/nokogiri/tmp/i686-redhat-linux/ports/libxml2/2.8.0/libxml2-2.8.0/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/invitado/rails_projects/sample_app/vendor/bundle/gems/nokogiri-1.6.1/ports/i686-redhat-linux/libxml2/2.8.0/lib"
+XML2_LIBS="-lxml2 -lz   -lm "
+XML2_INCLUDEDIR="-I/home/invitado/rails_projects/sample_app/vendor/bundle/gems/nokogiri-1.6.1/ports/i686-redhat-linux/libxml2/2.8.0/include/libxml2"
+MODULE_VERSION="xml2-2.8.0"
+
---
+++ vendor/bundle/gems/nokogiri-1.6.1/ports/i686-redhat-linux/libxslt/1.1.26/lib/xsltConf.sh
@@ -0,0 +1,7 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/home/invitado/rails_projects/sample_app/vendor/bundle/gems/nokogiri-1.6.1/ports/i686-redhat-linux/libxslt/1.1.26/lib"
+XSLT_LIBS="-lxslt  -L/home/invitado/rails_projects/sample_app/vendor/bundle/gems/nokogiri-1.6.1/ports/i686-redhat-linux/libxml2/2.8.0/lib -lxml2 -lz -lm -lm"
+XSLT_INCLUDEDIR="-I/home/invitado/rails_projects/sample_app/vendor/bundle/gems/nokogiri-1.6.1/ports/i686-redhat-linux/libxslt/1.1.26/include"
+MODULE_VERSION="xslt-1.1.26"
