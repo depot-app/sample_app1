57d97d24debe95cf861876b366a0787362b2fb2d
Juan Ruiz <invitado@perdidita.(none)>
2014-05-05 12:11:04 +0100

    Add user microposts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -40,6 +40,7 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
 subject { page }
 
 describe "Home page" do
@@ -47,7 +48,24 @@ describe "Home page" do
   it { should have_content('Sample App') }
   it { should have_title("Ruby on Rails Tutorial Sample App") }
   it { should_not have_title('| Home') }
-end
+
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        sign_in user
+        visit root_path
+      end
+
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          expect(page).to have_selector("li##{item.id}", text: item.content)
+        end
+      end
+    end
+  end
+
   describe "Help page" do
     it "should have the h1 'Help'" do
       visit root_path
